a32e79030904ca13cddcfc3f083db460a5da85ad
luaXoXo <79286488+luaXoXo@users.noreply.github.com>
2024-10-30 14:17:02 -0300

    base html

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>site</title>
+</head>
+<body>
+    
+<header>
+    <h1>site exemplo</h1>
+    <nav>
+        <a href="">Home</a>
+        <a href="">Equipe</a>
+        <a href="">Serviços</a>
+    </nav>
+</header>
+ 
+<main>
+    <article>
+        <h2>Bem-vindo(a)</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ipsam eaque molestias rem neque atque laboriosam eum voluptatibus voluptates ut earum optio corporis veritatis, dolore adipisci nobis ratione. Maiores, id.</p>
+    </article>
+</main>
+
+<footer>
+    <h2>Site criado por eu(lua)</h2>
+</footer>
+</body>
+</html>
